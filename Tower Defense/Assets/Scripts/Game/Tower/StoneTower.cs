47f28af4a187ca982adc562df36cf2a62e428572
SLCTECH\zayotte <zachary.M.Ayotte@hotmail.com>
2018-11-17 16:45:53 -0500

    Tower Fire (Stone) (Completed)

--- Tower Defense/Assets/Scripts/Game/Tower/StoneTower.cs
+++ Tower Defense/Assets/Scripts/Game/Tower/StoneTower.cs
@@ -11,5 +11,7 @@ public class StoneTower : Tower {
         base.AttackEnemy();
 
         GameObject stone = (GameObject)Instantiate(stonePrefab, towerPieceToAim.position, Quaternion.identity);
+        stone.GetComponent<Stone>().enemyToFollow = targetEnemy;
+        stone.GetComponent<Stone>().damage = attackPower;
     }
 }
