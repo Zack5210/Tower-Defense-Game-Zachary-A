7da340a2b9a6eaff48cc5f6888adb7e5cad4da0e
SLCTECH\zayotte <zachary.M.Ayotte@hotmail.com>
2018-11-17 14:38:17 -0500

    Enemy Spawn Script And Wave Manager (Completed)

---
+++ Tower Defense/Assets/Scripts/Game/Enemy/Wave/EnemyWave.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class EnemyWave {
+
+    public int pathIndex;
+    public float startSpawnTimeInSeconds;
+    public float timeBetweenSpawnsInSeconds = 1f;
+    public List<GameObject> listofEnemies = new List<GameObject>();
+
+}
---
+++ Tower Defense/Assets/Scripts/Game/Enemy/Wave/WaveManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+    public static WaveManager Instance;
+    public List<EnemyWave> enemyWaves = new List<EnemyWave>();
+    private float elapsedTime = 0f;
+    private EnemyWave activeWave;
+    private float spawnCounter = 0f;
+    private List<EnemyWave> activatedWaves = new List<EnemyWave>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        SearchForWave();
+        UpdateActiveWave();
+    }
+
+    private void SearchForWave()
+    {
+        foreach (EnemyWave enemyWave in enemyWaves)
+        {
+            if (!activatedWaves.Contains(enemyWave) && enemyWave.startSpawnTimeInSeconds <= elapsedTime)
+            {
+                activeWave = enemyWave;
+                activatedWaves.Add(enemyWave);
+                spawnCounter = 0f;
+                break;
+            }
+        }
+    }
+
+    private void UpdateActiveWave()
+    {
+        if (activeWave != null)
+        {
+            spawnCounter += Time.deltaTime;
+
+            if (spawnCounter >= activeWave.timeBetweenSpawnsInSeconds)
+            {
+                spawnCounter = 0f;
+
+                if(activeWave.listofEnemies.Count != 0)
+                {
+                    GameObject enemy = (GameObject)Instantiate(activeWave.listofEnemies[0], WayPointManager.Instance.
+                        GetSpawnPosition(activeWave.pathIndex), Quaternion.identity);
+
+                    enemy.GetComponent<Enemy>().pathIndex = activeWave.pathIndex;
+
+                    activeWave.listofEnemies.RemoveAt(0);
+                }
+                else
+                {
+                    activeWave = null;
+                    if (activatedWaves.Count == enemyWaves.Count)
+                    {
+                        // All Waves are over
+                    }
+                }
+            }
+        }
+    }
+
+    public void StopSpawning()
+    {
+        elapsedTime = 0;
+        spawnCounter = 0;
+        activeWave = null;
+        activatedWaves.Clear();
+        enabled = false;
+    }
+}
