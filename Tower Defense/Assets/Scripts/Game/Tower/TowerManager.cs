90b9fe5af7bc8a6b0b96b1f20344681bef67eaee
SLCTECH\zayotte <zachary.M.Ayotte@hotmail.com>
2018-11-17 17:15:06 -0500

    Linking the UI (Completed)

---
+++ Tower Defense/Assets/Scripts/Game/Tower/TowerManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+[Serializable]
+public struct TowerCost
+{
+    public TowerType TowerType;
+    public int Cost;
+}
+
+
+public class TowerManager : MonoBehaviour
+{
+
+
+    public static TowerManager Instance;
+
+    public GameObject stoneTowerPrefab;
+    public GameObject fireTowerPrefab;
+    public GameObject iceTowerPrefab;
+
+    public List<TowerCost> TowerCosts = new List<TowerCost>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CreateNewTower(GameObject slotToFill, TowerType towerType)
+    {
+        switch (towerType)
+        {
+            case TowerType.Stone:
+                Instantiate(stoneTowerPrefab, slotToFill.transform.position,
+                Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case TowerType.Fire:
+                Instantiate(fireTowerPrefab, slotToFill.transform.position,
+                Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case TowerType.Ice:
+                Instantiate(iceTowerPrefab, slotToFill.transform.position,
+                Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+        }
+
+    }
+
+    public int GetTowerPrice(TowerType towerType)
+    {
+        return (from towerCost in TowerCosts
+                where towerCost.TowerType
+                == towerType
+                select towerCost.Cost).FirstOrDefault();
+    }
+
+
+}
